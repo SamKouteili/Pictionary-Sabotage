e03f14e26568a4343d326a44600992d9e80017ec
gabefighter99 <gabrielpetrov99@gmail.com>
2021-10-12 16:04:03 +0300

    empty

--- CONTRIBUTORS.md
+++
@@ -1,5 +0,0 @@
-Sam Kouteili, E0435486
-Jachym Putta, E0332960
-Alvin Shrestha, E035489
-Tram Hoang, E0188347
-Gabriel Petrov, E0191890
---
+++ app/src/main/java/ync/ysc3232/pictionary_sabotage/MainActivity.java
@@ -0,0 +1,14 @@
+package ync.ysc3232.pictionary_sabotage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
