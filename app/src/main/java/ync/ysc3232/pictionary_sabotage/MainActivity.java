2329a5622f84dbaa54bcc52e56ff33d59f29cc10
gabefighter99 <gabrielpetrov99@gmail.com>
2021-10-12 17:34:52 +0300

    play buttonconnects drawer

--- app/src/main/java/ync/ysc3232/pictionary_sabotage/MainActivity.java
+++ app/src/main/java/ync/ysc3232/pictionary_sabotage/MainActivity.java
@@ -2,7 +2,10 @@ package ync.ysc3232.pictionary_sabotage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.startGameButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DrawingActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
