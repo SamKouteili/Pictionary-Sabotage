15185767fd4d953a251c69fd71018a76ed2c4bc5
gabefighter99 <gabrielpetrov99@gmail.com>
2021-10-14 00:09:53 +0300

    alvin can't write a line

--- app/src/main/java/ync/ysc3232/pictionary_sabotage/MainActivity.java
+++ app/src/main/java/ync/ysc3232/pictionary_sabotage/MainActivity.java
@@ -8,13 +8,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAnalytics mFirebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         Button button = findViewById(R.id.startGameButton);
         button.setOnClickListener(new View.OnClickListener() {
