0e197c4f478e6d3e3b8188cfbaddb8b3e8bc1091
SamKouteili <samkouteili@gmail.com>
2021-11-08 11:17:51 -0500

    cleaned podium screen and made data listener static

--- app/src/main/java/ync/ysc3232/pictionary_sabotage/PodiumActivity.java
+++ app/src/main/java/ync/ysc3232/pictionary_sabotage/PodiumActivity.java
@@ -13,6 +13,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -73,10 +75,10 @@ public class PodiumActivity extends AppCompatActivity {
         TextView ptsl2 = findViewById(R.id.ptsl2);
         ImageView skull2 = findViewById(R.id.skull2);
 
-        rooms_database.addValueEventListener(new ValueEventListener() {
+        rooms_database.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                roomData = snapshot.child(roomId).getValue(RoomData.class);
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                roomData = task.getResult().child(roomId).getValue(RoomData.class);
 
                 for (String id : roomData.players.keySet()) {
                     String role = roomData.players.get(id);
@@ -97,7 +99,7 @@ public class PodiumActivity extends AppCompatActivity {
                 String name3 = allPlayers.get(2).getPlayerName();
                 String points3 = allPlayers.get(2).getScore() + " pts.";
 
-                if (!roomData.players.get(name1).equals("Saboteur")) {
+                if (allPlayers.get(0).getRole().equals("Saboteur")) {
                     winner1.setText(name1);
                     ptsw1.setText(points1);
                     winner2.setVisibility(View.INVISIBLE);
@@ -121,92 +123,7 @@ public class PodiumActivity extends AppCompatActivity {
                     ptsw2.setText(points3);
                 }
             }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.e("firebase", "Error getting existing room.");
-            }
         });
 
     }
-
-
-
-
-//        Button returnToMenuButton = findViewById(R.id.returnToMenuButton);
-//
-//        //Fetch the corresponding layout text boxes
-//        TextView fstPos = findViewById(R.id.firstPos);
-//        TextView sndPos = findViewById(R.id.secondPos);
-//        TextView trdPos = findViewById(R.id.thirdPos);
-//
-//        rooms_database.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                roomData = snapshot.child(roomId).getValue(RoomData.class);
-//
-//                for (String id : roomData.players.keySet()) {
-//                    String role = roomData.players.get(id);
-//                    int score = roomData.scores.get(id);
-//                    PlayerDbModel p = new PlayerDbModel(id, role, score);
-//                    allPlayers.add(p);
-//                }
-//
-//                if (allPlayers.isEmpty()) {
-//                    Log.e("Fetching Podium Data", "Podium Data empty");
-//                }
-//                allPlayers.sort(Comparator.comparing(PlayerDbModel::getScore));
-//                // Set the values to the winners
-//                String nameAndPoints1 = allPlayers.get(0).getPlayerName() +
-//                        "\n" + allPlayers.get(0).getScore() + " pts.";
-////                String nameAndPoints2 = allPlayers.get(1).getPlayerName() +
-////                        "\n" + allPlayers.get(1).getScore() + " pts.";
-////                String nameAndPoints3 = allPlayers.get(2).getPlayerName() +
-////                        "\n" + allPlayers.get(2).getScore() + " pts.";
-//
-//                fstPos.setText(nameAndPoints1);
-////                sndPos.setText(nameAndPoints2);
-////                trdPos.setText(nameAndPoints3);
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                Log.e("firebase", "Error getting existing room.");
-//            }
-//        });
-//
-//
-//        returnToMenuButton.setOnClickListener(view -> {
-//            Intent intent = new Intent(PodiumActivity.this, MainActivity.class);
-//            startActivity(intent);
-//        });
-//    }
-
 }
-
-//    /**
-//     * Fetches the players from the database, puts the values into the @allPlayers field.
-//     */
-//    private void fetchPlayers() {
-//        rooms_database.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                roomData = snapshot.child(roomId).getValue(RoomData.class);
-//
-//                for (String id : roomData.players.keySet()) {
-//                    String role = roomData.players.get(id);
-//                    int score = roomData.scores.get(id);
-//                    PlayerDbModel p = new PlayerDbModel(id, role, score);
-//                    allPlayers.add(p);
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                Log.e("firebase", "Error getting existing room.");
-//            }
-//        });
-//    }
-//
