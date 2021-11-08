a8cd20ca6b3b02a87d30818cb2f40abcc63c7db8
jachym <jachym.putta@u.yale-nus.edu.sg>
2021-11-08 16:58:29 +0800

    local unit testing

--- app/src/test/java/ync/ysc3232/pictionary_sabotage/ExampleUnitTest.java
+++ app/src/test/java/ync/ysc3232/pictionary_sabotage/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,4 +16,50 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void playerDbModelScoreTest (){
+        PlayerDbModel s = new PlayerDbModel("John", "Guesser", 31);
+        assert(s.getScore() == 31);
+        s.setScore(41);
+        assert(s.getScore() == 41);
+    }
+    @Test
+    public void playerDbModelNameTest (){
+        PlayerDbModel s = new PlayerDbModel("John", "Guesser", 31);
+        assert(s.getPlayerName().equals("John"));
+        s.setPlayerName("Bob");
+        assert(s.getPlayerName().equals("Bob"));
+    }
+
+    @Test
+    public void roomDataRoomIDTest () {
+        RoomData room = new RoomData("2402");
+        assertEquals(room.getRoomId(), "2402");
+        room.setRoomId("Potato");
+        assertEquals(room.getRoomId(), "Potato");
+    }
+
+    @Test
+    public void roomDataScoreTest () {
+        RoomData room = new RoomData("2402");
+        room.addPlayer("John", "Guesser");
+        room.addScoreTo("John");
+        room.addScoreTo("John");
+        room.addScoreTo("John");
+        Map<String, Integer> scores = room.getScores();
+        int score = scores.getOrDefault("John", 0);
+        assertEquals(score, 3);
+    }
+    @Test
+    public void roomDataSetPlayersTest () {
+        RoomData room = new RoomData("2402");
+        room.addPlayer("John", "Guesser");
+        assertFalse(room.isGameStarted());
+        Map<String, String> players = room.getPlayers();
+        String role = players.getOrDefault("John", "");
+        assert (role.equals("Guesser"));
+    }
+
+
 }
